d58b29813ea4271ef2cee7b54f079543da8d99f1
apernea <apernea31@gmail.com>
2025-08-02 16:36:47 +0300

    Defined Bap1AudioProcessorEditor class with basic JUCE AudioProcessorEditor structure

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#ifndef PLUGINEDITOR_H
-#define PLUGINEDITOR_H
+#include <juce_gui_basics/juce_gui_basics.h>
+#include "PluginProcessor.h"
 
+class Bap1AudioProcessorEditor : public juce::AudioProcessorEditor {
+public:
+    explicit Bap1AudioProcessorEditor(Bap1AudioProcessor &);
+    ~Bap1AudioProcessorEditor() override;
 
+    void paint(juce::Graphics &g) override;
+    void resized() override;
+private:
+    Bap1AudioProcessor& bap1_audio_processor;
 
-class PluginEditor {
-
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Bap1AudioProcessorEditor);
 };
 
-
-
-#endif //PLUGINEDITOR_H
