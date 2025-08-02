44943ba0fd44d0342a91647621e7dde9be806d19
apernea <apernea31@gmail.com>
2025-08-02 16:37:32 +0300

    Implemented Bap1AudioProcessorEditor with basic paint and resize methods

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "PluginEditor.h"
+#include "PluginProcessor.h"
+
+Bap1AudioProcessorEditor::Bap1AudioProcessorEditor(Bap1AudioProcessor& p)
+    : AudioProcessorEditor (&p), bap1_audio_processor (p)
+{
+    setSize(800, 800);
+}
+
+Bap1AudioProcessorEditor::~Bap1AudioProcessorEditor() = default;
+
+void Bap1AudioProcessorEditor::paint(juce::Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+}
+
+void Bap1AudioProcessorEditor::resized() {}
